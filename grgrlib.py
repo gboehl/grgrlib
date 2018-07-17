108ca6872c5e1c02acd9825192f21b5bd149a87f
gboehl <admin@gregorboehl.com>
2018-07-17 19:21:35 +0200

    Add fast0 function.

--- grgrlib.py
+++ grgrlib.py
@@ -26,6 +26,7 @@ def sorter(x, y):
     return out
 
 def get_sys(mod, par):
+
     A   = mod.AA(par)
     B   = mod.BB(par)
     C   = mod.CC(par)
@@ -47,7 +48,30 @@ def get_sys(mod, par):
     if not nl.det(P1) or not nl.det(M1):
         print('Singularities! P,M:', nl.det(P1), nl.det(M1))
 
+    # active_ind      = ~np.isclose(Z.T[ind],0).all(axis=0)
+
+    # if not np.allclose( PP[~ind]@Z.T[:,~active_ind], 0 ):
+    if not np.allclose( PP[~ind][:,~ind], 0 ):
+        print('Apparent dependency on redundant future vals!')
+
     N   = nl.inv(M1) @ P1
 
-    return N
+    # return N
+    return N, MM, PP, Q, Z, ind
+
+def re_bc(N, d_endo):
+
+    eigvals, eigvecs 	= nl.eig(nl.inv(N))
+
+    idx             	= np.abs(eigvals).argsort()[::-1]
+
+    eigvecs      		= nl.inv(eigvecs[:,idx])     
+
+    res     = nl.inv(eigvecs[:d_endo,:d_endo]) @ eigvecs[:d_endo,d_endo:]
+
+    return -res.real
+
+def fast0(A):
+
+    return np.isclose(A, 0)
 
