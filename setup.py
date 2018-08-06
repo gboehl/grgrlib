4fbb639701fc31d72b8f243e016e4d98d4c87720
Gregor Boehl <admin@gregorboehl.com>
2018-08-06 23:22:49 +0200

    adding dependencies

--- setup.py
+++ setup.py
@@ -7,17 +7,9 @@ setup(
         author_email='admin@gregorboehl.com',
         description='Various functions & libraries for economic analysis',
         packages = find_packages(),
-        # install_requires=[
-            # 'pandas',
-            #'slycot',
-            # 'sympy',
-            # 'scipy',
-            # 'numpy',
-         # ],
-        # py_modules=['grgrlib','pyzlb'],
-        # package_data = {'grgrlib':
-                        # ['base.py',
-                         # 'pyzlb.py']},
-        # package_dir = {'':'grgrlib'},
-        # py_modules=['grgrlib'],
+        install_requires=[
+            'sympy',
+            'scipy',
+            'numpy',
+         ],
    )
