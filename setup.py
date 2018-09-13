33b05be6dc193e61a5659e916f3fc30283787fa7
Gregor Boehl <admin@gregorboehl.com>
2018-09-13 22:37:42 +0200

    splitting packages

--- setup.py
+++ setup.py
@@ -2,20 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'grgrlib',
-        # version = '0.0.0alpha',
         version = 'alpha',
         author='Gregor Boehl',
         author_email='admin@gregorboehl.com',
-        description='Various functions & libraries for economic analysis',
+        description='Various insanly helpful functions',
         packages = find_packages(),
         install_requires=[
             'sympy',
+            'matplotlib',
             'scipy',
             'numpy',
-            'pandas',
-            'filterpy-dsge',
-            'emcee',
-            'pathos',
-            'pydsge',
          ],
    )
