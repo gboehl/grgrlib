fd3e55f47a30b0adfb0d41976f02a994aca8c7ae
Gregor Boehl <admin@gregorboehl.com>
2019-09-04 14:38:49 +0200

    remove debugging

--- grgrlib/mp_tools.py
+++ grgrlib/mp_tools.py
@@ -12,14 +12,17 @@ class desert_island(object):
 
     def __init__(self, pool_size=None):
 
-        if pool_size is None:
-            pool_size = pathos.multiprocessing.cpu_count()
-        elif pool_size == 1:
+        if pool_size == 1:
+
             desert_island.pool = None
             desert_island.pool_size = 1
+
             return
 
         import pathos
+
+        if pool_size is None:
+            pool_size = pathos.multiprocessing.cpu_count()
 
         desert_island.pool_size = pool_size
         desert_island.pool = pathos.multiprocessing.Pool(pool_size)
@@ -29,7 +32,6 @@ class desert_island(object):
 
     def run_evolve(self, algo, pop):
 
-        print('hier')
         if desert_island.pool is None:
 
             new_pop = algo.evolve(pop)
