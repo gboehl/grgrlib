fdda8cd79fb0db5d16ab31099e680dd6920e806d
Gregor Boehl <admin@gregorboehl.com>
2021-09-19 14:30:40 +0200

    add multiprocessing

---
+++ grgrlib/multiprocessing.py
@@ -0,0 +1,45 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+
+def serializer(func):
+    """Dirty hack that transforms the non-serializable function to a serializable one (when using dill)
+    ...
+    Don't try that at home!
+    """
+
+    fname = func.__name__
+    exec("dark_%s = func" % fname, locals(), globals())
+
+    def vodoo(*args, **kwargs):
+        return eval("dark_%s(*args, **kwargs)" % fname)
+
+    return vodoo
+
+
+class JoblibPoolDummy(object):
+    """joblib Parallel workers pool pretending to behave like a multiprocessing pool
+    """
+
+    def __init__(self, func=None, njobs=None, **kwargs):
+
+        if njobs is None:
+            self.njobs = multiprocessing.cpu_count()
+        else:
+            self.njobs = njobs
+
+        self.func = func
+        self.parallel = Parallel(n_jobs=self.njobs, **kwargs)
+        self.parallel.__enter__()
+
+    def map(self, func, iterable):
+        return self.parallel(map(delayed(func), iterable))
+
+    def vectorized_func(self, iterable):
+        return self.parallel(delayed(self.func)(val) for val in list(iterable))
+
+    def close(self):
+        self.parallel.__exit__(None, None, None)
