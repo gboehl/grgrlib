c9c4da9d8ea6f616e27aa02e90bda6cb577d0e07
Gregor Boehl <admin@gregorboehl.com>
2021-12-14 18:26:15 +0100

    add sdw parser

---
+++ grgrlib/data_tools.py
@@ -0,0 +1,22 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+
+def sdw_parser(x):
+    """Tries to encode the date format used by the ECB statistical data warehouse (SDW). To be used for the argument `date_parser` in pandas `read_csv`"""
+
+    try:
+        return pd.to_datetime(x+'0', format='%YW%W%w')
+    except:
+        pass
+
+    try:
+        return pd.to_datetime(x, format='%Y%b')
+    except:
+        pass
+
+    raise ValueError('Could not find a format for %s.' %s)
+
