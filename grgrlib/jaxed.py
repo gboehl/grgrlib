25108a8b210e3f2dd242a23f9771fca9a8c3e51a
Gregor Boehl <admin@gregorboehl.com>
2022-02-22 12:39:15 +0100

    add jaxed newton method

---
+++ grgrlib/jaxed.py
@@ -0,0 +1,68 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import jax
+import scipy.sparse as ssp
+import scipy as sp
+
+
+def newton_jax(func, init, maxit=30, tol=1e-8, sparse=False, verbose=False):
+    """Newton method for root finding using automatic differenciation with jax. The argument `func` must be jittable with jax.
+
+    ...
+
+    Parameters
+    ----------
+    func : callable 
+        Function f for which f(x)=0 should be found. Must be jittable with jax
+    init : array
+        Initial values of x
+    maxit : int, optional
+        Maximum number of iterations
+    tol : float, optional
+        Random seed. Defaults to 0
+    sparse : bool, optional
+        Whether to use a sparse solver
+    verbose : bool, optional
+        Whether to display messages
+
+    Returns
+    -------
+    res: dict 
+        A dictionary of results similar to the output from scipy.optimize.root
+    """
+
+    jac = jax.jacfwd(func)
+
+    res = {}
+    cnt = 0
+    xi = init.copy()
+
+    while True:
+        cnt += 1
+        xold = xi.copy()
+        if sparse:
+            xi -= ssp.linalg.spsolve(ssp.csr_matrix(jac(xi)), func(xi))
+        else:
+            xi -= sp.linalg.solve(jac(xi), func(xi))
+        eps = jax.numpy.abs(xi - xold).max()
+
+        if verbose:
+            print(f'    Iteration {cnt:3d} | max error {eps:.2e}')
+
+        if cnt == maxit:
+            res['success'] = False
+            res['message'] = f"Maximum number of {maxit} iterations reached."
+            break
+
+        if eps < 1e-8:
+            res['success'] = True
+            res['message'] = "The solution converged."
+            break
+
+        if jax.numpy.isnan(eps):
+            raise Exception('Newton method returned `NaN`')
+
+    res['x'], res['fun'], res['niter'] = xi, func(xi), cnt
+
+    return res
