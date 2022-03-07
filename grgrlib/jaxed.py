67ddd7db353fe0d8cd8e92f0e16fe13f71031c87
Gregor Boehl <admin@gregorboehl.com>
2022-03-07 17:41:31 +0100

    add pure jax newton method

--- grgrlib/jaxed.py
+++ grgrlib/jaxed.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import jax
+import jax.numpy as np
 import time
 import scipy.sparse as ssp
 
@@ -13,7 +14,7 @@ def newton_jax(func, init, jac=None, maxit=30, tol=1e-8, sparse=False, verbose=F
 
     Parameters
     ----------
-    func : callable 
+    func : callable
         Function f for which f(x)=0 should be found. Must be jittable with jax
     init : array
         Initial values of x
@@ -30,7 +31,7 @@ def newton_jax(func, init, jac=None, maxit=30, tol=1e-8, sparse=False, verbose=F
 
     Returns
     -------
-    res: dict 
+    res: dict
         A dictionary of results similar to the output from scipy.optimize.root
     """
 
@@ -38,7 +39,7 @@ def newton_jax(func, init, jac=None, maxit=30, tol=1e-8, sparse=False, verbose=F
 
     if jac is None:
         if sparse:
-            jac = lambda x: ssp.csr_array(jax.jacfwd(func)(x))
+            def jac(x): return ssp.csr_array(jax.jacfwd(func)(x))
         else:
             jac = jax.jacfwd(func)
 
@@ -59,21 +60,76 @@ def newton_jax(func, init, jac=None, maxit=30, tol=1e-8, sparse=False, verbose=F
 
         if verbose:
             ltime = time.time() - st
-            print(f'    Iteration {cnt:3d} | max error {eps:.2e} | lapsed %ss' %str(ltime)[:6])
+            print(
+                f'    Iteration {cnt:3d} | max error {eps:.2e} | lapsed %ss' % str(ltime)[:6])
 
         if cnt == maxit:
             res['success'] = False
             res['message'] = f"Maximum number of {maxit} iterations reached."
             break
 
-        if eps < 1e-8:
+        if eps < tol:
             res['success'] = True
             res['message'] = "The solution converged."
             break
 
         if jax.numpy.isnan(eps):
-            raise Exception('Newton method returned `NaN`')
+            raise Exception('Newton method returned `NaN` in iter %s' % cnt)
 
     res['x'], res['fun'], res['niter'] = xi, func(xi), cnt
 
     return res
+
+
+def newton_jax_pure_raw(func, init, jac=None, maxit=30, tol=1e-8):
+    """Newton method for root finding using automatic differenciation with jax BUT running in pure jitted jax. The argument `func` must be jittable with jax. Remember to check the error flags!
+
+    ...
+
+    Parameters
+    ----------
+    func : callable
+        Function f for which f(x)=0 should be found. Must be jittable with jax
+    init : array
+        Initial values of x
+    jac : callable, optional
+        Funciton that returns the jacobian. If not provided, jax.jacfwd is used
+    maxit : int, optional
+        Maximum number of iterations
+    tol : float, optional
+        Random seed. Defaults to 0
+
+    Returns
+    -------
+    res: (xopt, fopt, niter, success)
+    """
+
+    if jac is None:
+        jac = jax.jacfwd(func)
+
+    xi = np.array(init)
+
+    def cond_func(tain):
+        xi, xold, cnt = tain
+        eps = np.abs(xi - xold).max()
+
+        cond = cnt < maxit
+        cond &= eps > tol
+        cond &= ~jax.numpy.isnan(eps)
+
+        return cond
+
+    def body_func(tain):
+        (xi, _, cnt) = tain
+        cnt += 1
+        xold = xi
+        xi -= jax.scipy.linalg.solve(jac(xi), func(xi))
+        return (xi, xold, cnt)
+
+    tain = jax.lax.while_loop(cond_func, body_func, (xi, xi + 1, 0))
+    eps = np.abs(tain[0] - tain[1]).max()
+
+    return tain[0], func(tain[0]), tain[2], eps < tol
+
+
+newton_jax_pure = jax.jit(newton_jax_pure_raw, static_argnums=(0, 2, 3, 4))
