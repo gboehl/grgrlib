2f637723f372454ce3363925b9595cef93ed2eba
Gregor Boehl <admin@gregorboehl.com>
2022-04-28 16:52:39 +0200

    make np compatible

--- grgrlib/h5.py
+++ grgrlib/h5.py
@@ -11,8 +11,8 @@ def init(path, dataset_name, data, compression='gzip'):
     """
 
     f = h5py.File(path, 'a')
-    f.create_dataset(dataset_name, data=data[np.newaxis],
-                     compression=compression, chunks=True, maxshape=(None, *data.shape))
+    f.create_dataset(dataset_name, data=np.array(data)[
+                     np.newaxis], compression=compression, chunks=True, maxshape=(None, *np.shape(data)))
     f.close()
 
     return
@@ -33,10 +33,31 @@ def append(path_or_h5py, dataset_names, data):
 
     # batch append assumes that all datasets have the same lenght!
     # if this is irrelevant, just call append independently for each dataset
-    oldlenght = f[dataset_names[0]].shape[0]
+    oldlenght = np.shape(f[dataset_names[0]])[0]
     for i, name in enumerate(dataset_names):
         f[name].resize(oldlenght + 1, axis=0)
         f[name][oldlenght] = data[i]
+
+    f.close()
+
+    return
+
+
+def write_at(path_or_h5py, dataset_names, index, data):
+    """Append to a dataset. Batch append takes care that the append to each dataset has the same index!
+    """
+
+    if isinstance(path_or_h5py, str):
+        f = h5py.File(path_or_h5py, 'a')
+    else:
+        f = path_or_h5py
+
+    if isinstance(dataset_names, str):
+        dataset_names = dataset_names,
+        data = data,
+
+    for i, name in enumerate(dataset_names):
+        f[name][index] = data[i]
 
     f.close()
 
