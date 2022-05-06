0ed11e1572624c7b0499e082ec5bc6e2a2833699
Gregor Boehl <admin@gregorboehl.com>
2022-05-06 20:13:59 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description_content_type="text/x-rst",
     url="https://github.com/gboehl/grgrlib",
     name='grgrlib',
-    version='0.1.4',
+    version='0.1.5',
     author='Gregor Boehl',
     author_email='admin@gregorboehl.com',
     license='MIT',
