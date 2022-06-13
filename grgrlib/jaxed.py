8d4868939268d5bca6c946cff2ea682fc40fba42
Gregor Boehl <admin@gregorboehl.com>
2022-06-13 10:01:08 +0200

    add jacrev_and_val

--- grgrlib/jaxed.py
+++ grgrlib/jaxed.py
@@ -9,8 +9,8 @@ import scipy.sparse as ssp
 from numpy import ndarray, isnan
 from .plots import spy
 from jax.experimental.host_callback import id_print as jax_print
-from jax._src.api import (_check_callable, _check_input_dtype_jacfwd, _check_output_dtype_jacfwd, _ensure_index,
-                          _jvp, _std_basis, _jacfwd_unravel, lu, argnums_partial, tree_map, partial, Callable, Sequence, Union, vmap)
+from jax._src.api import (_check_callable, _check_input_dtype_jacfwd, _check_input_dtype_jacrev, _check_output_dtype_jacfwd, _check_output_dtype_jacrev, _ensure_index,
+                          _jvp, _vjp, _std_basis, _jacfwd_unravel, _jacrev_unravel, lu, argnums_partial, tree_map, tree_structure, tree_transpose, partial, Callable, Sequence, Union, vmap)
 
 
 def jacfwd_and_val(fun: Callable, argnums: Union[int, Sequence[int]] = 0,
@@ -19,7 +19,7 @@ def jacfwd_and_val(fun: Callable, argnums: Union[int, Sequence[int]] = 0,
   jax._src.api.
 
     Args:
-      fun: Function whose Jacobian is to be computed.
+      fun: Function whose value and Jacobian is to be computed.
       argnums: Optional, integer or sequence of integers. Specifies which
         positional argument(s) to differentiate with respect to (default ``0``).
       has_aux: Optional, bool. Indicates whether ``fun`` returns a pair where the
@@ -51,6 +51,55 @@ def jacfwd_and_val(fun: Callable, argnums: Union[int, Sequence[int]] = 0,
         tree_map(partial(_check_output_dtype_jacfwd, holomorphic), y)
         example_args = dyn_args[0] if isinstance(argnums, int) else dyn_args
         jac_tree = tree_map(partial(_jacfwd_unravel, example_args), y, jac)
+        if not has_aux:
+            return y, jac_tree
+        else:
+            return y, jac_tree, aux
+
+    return jacfun
+
+
+def jacrev_and_val(fun: Callable, argnums: Union[int, Sequence[int]] = 0,
+                   has_aux: bool = False, holomorphic: bool = False, allow_int: bool = False) -> Callable:
+    """Value and Jacobian of ``fun`` evaluated row-by-row using reverse-mode AD.
+
+    Args:
+      fun: Function whose value and Jacobian are to be computed.
+      argnums: Optional, integer or sequence of integers. Specifies which
+        positional argument(s) to differentiate with respect to (default ``0``).
+      has_aux: Optional, bool. Indicates whether ``fun`` returns a pair where the
+        first element is considered the output of the mathematical function to be
+        differentiated and the second element is auxiliary data. Default False.
+      holomorphic: Optional, bool. Indicates whether ``fun`` is promised to be
+        holomorphic. Default False.
+      allow_int: Optional, bool. Whether to allow differentiating with
+        respect to integer valued inputs. The gradient of an integer input will
+        have a trivial vector-space dtype (float0). Default False.
+
+    Returns:
+      A function with the same arguments as ``fun``, that evaluates the value and Jacobian of
+      ``fun`` using reverse-mode automatic differentiation. If ``has_aux`` is True
+      then a pair of (jacobian, auxiliary_data) is returned.
+    """
+    _check_callable(fun)
+
+    def jacfun(*args, **kwargs):
+        f = lu.wrap_init(fun, kwargs)
+        f_partial, dyn_args = argnums_partial(f, argnums, args,
+                                              require_static_args_hashable=False)
+        tree_map(partial(_check_input_dtype_jacrev,
+                 holomorphic, allow_int), dyn_args)
+        if not has_aux:
+            y, pullback = _vjp(f_partial, *dyn_args)
+        else:
+            y, pullback, aux = _vjp(f_partial, *dyn_args, has_aux=True)
+        tree_map(partial(_check_output_dtype_jacrev, holomorphic), y)
+        jac = vmap(pullback)(_std_basis(y))
+        jac = jac[0] if isinstance(argnums, int) else jac
+        example_args = dyn_args[0] if isinstance(argnums, int) else dyn_args
+        jac_tree = tree_map(partial(_jacrev_unravel, y), example_args, jac)
+        jac_tree = tree_transpose(tree_structure(
+            example_args), tree_structure(y), jac_tree)
         if not has_aux:
             return y, jac_tree
         else:
