e19558407112fb45315baf799a4b6b1ef0558078
Gregor Boehl <admin@gregorboehl.com>
2022-07-09 10:48:08 +0200

    separate numba usage

--- grgrlib/linalg.py
+++ grgrlib/linalg.py
@@ -4,7 +4,6 @@
 import numpy as np
 import scipy.linalg as sl
 import matplotlib.pyplot as plt
-from numba import njit
 
 
 def H(arr):
@@ -108,42 +107,6 @@ def givens_rotation(a, b):
     return (c, s)
 
 
-@njit(cache=True, nogil=True, fastmath=True)
-def householder_reflection(x):
-    alpha = x[0]
-    s = np.linalg.norm(x[1:]) ** 2
-    v = x.copy()
-
-    if s == 0:
-        tau = 0.0
-    else:
-        t = np.sqrt(alpha ** 2 + s)
-        v[0] = alpha - t if alpha <= 0 else -s / (alpha + t)
-
-        tau = 2 * v[0] ** 2 / (s + v[0] ** 2)
-        v /= v[0]
-
-    return v, tau
-
-
-@njit(cache=True, nogil=True, fastmath=True)
-def householder_reflection_right(x):
-    alpha = x[-1]
-    s = np.linalg.norm(x[:-1]) ** 2
-    v = x.copy()
-
-    if s == 0:
-        tau = 0.0
-    else:
-        t = np.sqrt(alpha ** 2 + s)
-        v[-1] = alpha - t if alpha <= 0 else -s / (alpha + t)
-
-        tau = 2 * v[-1] ** 2 / (s + v[-1] ** 2)
-        v /= v[-1]
-
-    return v, tau
-
-
 def qr_gr(A):
     """Perform QR decomposition of matrix A using Givens rotation."""
     num_rows, num_cols = np.shape(A)
@@ -220,158 +183,6 @@ def ql(M):
     """Perform QL decomposition of matrix A using scipy"""
     r, q = sl.rq(M.T)
     return q.T, r.T
-
-
-@njit(cache=True, nogil=True)
-def shredder_basic(M, tol, verbose):
-
-    m, n = M.shape
-    Q = np.eye(m)
-
-    j = 0
-
-    for i in range(min(m, n)):
-
-        while j < n:
-
-            a = np.ascontiguousarray(Q.T[i:]) @ np.ascontiguousarray(M[:, j])
-            do = False
-            for ia in a:
-                if abs(ia) > tol:
-                    do = True
-                    break
-
-            if do:
-                # apply Householder transformation
-                v, tau = householder_reflection(a)
-
-                H = np.identity(m - i)
-                H -= tau * np.outer(v, v)
-
-                Q[:, i:] = np.ascontiguousarray(Q[:, i:]) @ H
-
-                if verbose:
-                    print("...shredding row", j, "and col", i)
-                break
-
-            else:
-                j += 1
-
-    return Q
-
-
-@njit(cache=True, nogil=True)
-def shredder_pivoting(M, tol, verbose):
-    """The DS-decomposition with pivoting"""
-
-    m, n = M.shape
-    M = M.copy().astype(np.float64)
-    Q = np.eye(m)
-    P = np.arange(n)
-
-    j = 0
-
-    for i in range(min(m, n)):
-
-        # pivoting loop
-        k = i
-        while True:
-            if k == n:
-                break
-            if np.any(np.abs(M[i:, k]) > tol):
-                if k != i:
-                    # move columns
-                    M[:, np.array([i, k])] = M[:, np.array([k, i])]
-                    P[np.array([i, k])] = P[np.array([k, i])]
-                break
-            k += 1
-
-        while j < n:
-            if np.any(np.abs(M[i:, j]) > tol):
-
-                # apply Householder transformation
-                a = M[i:, j]
-                v, tau = householder_reflection(a)
-
-                H = np.identity(m - i)
-                H -= tau * np.outer(v, v)
-                M[i:] = H @ M[i:]
-                Q[:, i:] = np.ascontiguousarray(Q[:, i:]) @ H
-
-                if verbose:
-                    print("...shredding row", j, "and col", i)
-                break
-
-            else:
-                j += 1
-
-    return Q, M, P
-
-
-@njit(cache=True, nogil=True)
-def shredder_non_pivoting(M, tol, verbose):
-    """The DS-decomposition with anti-pivoting"""
-
-    m, n = M.shape
-    M = M.copy().astype(np.float64)
-    Q = np.eye(m)
-    P = np.arange(n)
-
-    j = 0
-
-    for i in range(min(m, n)):
-
-        # anti-pivoting
-        # for each i move those columns forward that do not need transformation
-
-        for k in range(j, n):
-            if np.all(np.abs(M[i:, k]) < tol):
-                # move columns
-                if k != j:
-                    M[:, np.array([j, k])] = M[:, np.array([k, j])]
-                    P[np.array([j, k])] = P[np.array([k, j])]
-
-                if verbose:
-                    print("...switching rows", j, "and", k)
-                j += 1
-
-        while j < n:
-            if np.any(np.abs(M[i:, j]) > tol):
-
-                # apply Householder transformation
-                a = M[i:, j]
-                v, tau = householder_reflection(a)
-
-                H = np.identity(m - i)
-                H -= tau * np.outer(v, v)
-                M[i:] = H @ M[i:]
-                Q[:, i:] = np.ascontiguousarray(Q[:, i:]) @ H
-
-                if verbose:
-                    print("...shredding row", j, "and col", i)
-                break
-
-            else:
-                j += 1
-
-    return Q, M, P
-
-
-def shredder(M, pivoting=None, tol=1e-11, verbose=False):
-    """The QS decomposition from "Efficient Solution of Models with Occasionally Binding Constraints" (Gregor Boehl)
-
-    The QS decomposition uses Householder reflections to bring a system in the row-echolon form. This is a dispatcher for the sub-functions.
-    """
-
-    if pivoting is None:
-        q = shredder_basic(M, tol, verbose)
-        return q, q.T @ M
-    elif pivoting:
-        return shredder_pivoting(M, tol, verbose)
-    elif not pivoting:
-        return shredder_non_pivoting(M, tol, verbose)
-    else:
-        raise NotImplementedError
 
 
 def nearest_psd(A, eps=0):
--- grgrlib/stats.py
+++ grgrlib/stats.py
@@ -2,42 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from numba import njit
-
-_cond = 1e-9
-
-
-@njit(cache=True)
-def psd_func(M):
-    """Compute the symmetric eigendecomposition
-
-    Note that eigh takes care of array conversion, chkfinite, and assertion that the matrix is square.
-    """
-
-    s, u = np.linalg.eigh(M)
-    eps = _cond * np.max(np.abs(s))
-    d = s[s > eps]
-
-    s_pinv = [0 if abs(x) <= eps else 1 / x for x in s]
-    s_pinv_np = np.array(s_pinv)
-    U = np.multiply(u, np.sqrt(s_pinv_np))
-
-    return U, np.sum(np.log(d)), len(d)
-
-
-@njit(cache=True)
-def mvn_logpdf(x, mean, cov):
-    """log-PDF of multivariate normal"""
-
-    _LOG_2PI = np.log(2 * np.pi)
-
-    dim = mean.shape[0]
-    prec_U, log_det_cov, rank = psd_func(cov)
-    dev = x - mean
-    maha = np.sum(np.square(np.dot(dev, prec_U)), axis=-1)
-    out = -0.5 * (rank * _LOG_2PI + log_det_cov + maha)
-
-    return out
 
 
 def percentile(x, q=0.01):
